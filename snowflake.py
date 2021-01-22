de818b5e1ef45e531b90b91ec56c8d5cd972fcbd
James <jman900000@gmail.com>
2021-01-22 13:09:57 -0500

    snow

--- snowflake.py
+++ snowflake.py
@@ -208,6 +208,86 @@ def randHillTops(cartesian):
         
 
 
+def carpets(n):
+    win = makeWin()
+    for w in range(n):
+        locx = r.randrange(0, 280)
+        locy = r.randrange(0, 280)
+        dimx = r.randrange(10, 30)
+        dimy = r.randrange(4, 15)
+        dimz = r.randrange(4, 15)
+        for x in range(dimx):
+            for y in range(dimy):
+                for z in range(dimz):
+                    #dot = Point(locx+5*x+2*z, locy+5*y)+2*z)
+                    dot = Point(locx+5*x+2*z, locy+5*math.sin(5*x)+2*z)
+                    dot.draw(win)
+
+
+def dotCube(n):
+    win = makeWin()
+    angle = [1,1] #default angle is [2, 2]
+    for w in range(n):
+        locx = r.randrange(0, 280)
+        locy = r.randrange(0, 280)
+        dimx = r.randrange(4, 15)
+        dimy = r.randrange(4, 15)
+        dimz = r.randrange(4, 15)
+        for x in range(dimx):
+            for y in range(dimy):
+                for z in range(dimz):
+                    dot = Point(locx+5*x+angle[0]*z, locy+5*y+angle[1]*z)
+                    #dot = Point(locx+5*x+2*z, locy+5*math.sin(5*x)+2*z)
+                    dot.draw(win)
+        for x in range (dimx*5):
+            #front box
+            pt = Point(locx+x, locy+(dimy*5))
+            pt.draw(win)
+            pt = Point(locx+x,locy)
+            pt.draw(win)
+            #back box
+            pt = Point(locx+x+(dimz),locy+(dimy*5)+(dimz))
+            pt.draw(win)
+            #pt = Point(locx, locy+dimy
+        for y in range (dimy*5):
+            #front box
+            pt = Point(locx+(dimx*5),locy+y)
+            pt.draw(win)
+            pt = Point(locx, locy+y)
+            pt.draw(win)
+            #back box
+        for z in range (dimz*2):
+            pt = Point(locx+(dimx*5)+(z/5), locy+(5*dimy)+(z/5))
+            pt.draw(win)
+            #pt = Point(locx+(dimx*5)+(z/4)-1, locy+(dimy*5)+(z/4))
+            #pt.draw(win)
+
+def cubeSpin(n):
+    win = makeWin()
+    res = [300,300]
+    locx = res[0]/2
+    locy = res[1]/2
+    angle = [2,2] #default angle is [2, 2]
+    #each iteration of w is a slightly different angle of the cube
+    rect = Rectangle(Point(0,0),Point(300,300))
+    rect.setFill("white")
+    for w in range(n):
+        rect.draw(win)
+        dim = 5
+        for x in range(dim):
+            for y in range(dim):
+                for z in range(dim):
+                    dot = Point(locx+5*x+angle[0]*z, locy+5*y+angle[1]*z)
+                    #dot = Point(locx+5*x+2*z, locy+5*math.sin(5*x)+2*z)
+                    dot.draw(win)
+        #clear window
+        #increase angle. maybe use sin waves for this so they
+        angle = [angle[0]-math.sin(w),angle[1]+math.cos(w)]
+
+
+ 
+
+
 def newShit(n):
     i = 300
     j = 300
@@ -278,9 +358,15 @@ if __name__ == '__main__':
             cartToPolarTest(x, y)
         elif(cont == 'o'):
             snowflake()
-            
+        elif(cont == 'd'):
+            cubes = int(input("enter number of cubes: "))
+            dotCube(cubes)
+        elif(cont == 'dd'):
+            carpets(10)
+        elif(cont == 'cs'):
+            cubeSpin(10)
 
-        cont = input("MENU\n o - snowflake\n t - cartToPolarTest\n h - random hill tops\n a - circle draw\n n - fib\n c - random circles\n q - quit\n p - point line\n r - random shape\n")
+        cont = input("MENU\n cs - cube spin\n dd - carpets\n d - dotcube\n o - snowflake\n t - cartToPolarTest\n h - random hill tops\n a - circle draw\n n - fib\n c - random circles\n q - quit\n p - point line\n r - random shape\n")
     print("Have a cool chill day!")
 
 
